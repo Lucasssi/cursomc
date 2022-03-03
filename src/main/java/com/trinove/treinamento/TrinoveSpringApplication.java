8d34e1abb292f865098242142ecdac99e64a2792
Lucas Assis Lima <lucasssi89@gmail.com>
2022-03-02 22:55:24 -0300

    Projeto criado

---
+++ src/main/java/com/trinove/treinamento/TrinoveSpringApplication.java
@@ -0,0 +1,13 @@
+package com.trinove.treinamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrinoveSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrinoveSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trinove/treinamento/TrinoveSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trinove.treinamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrinoveSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
